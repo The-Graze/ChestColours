f80e37a674dea42ba81456ac5757ea04718fe66c
Graze <Graze@DESKTOP-6KJ7H29>
2022-05-27 02:58:23 +0100

    Add project files.

---
+++ ChestColours/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace ChestColours
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.graze.gorillatag.chestcolours";
+        public const string Name = "ChestColours";
+        public const string Version = "1.0.0";
+    }
+}
